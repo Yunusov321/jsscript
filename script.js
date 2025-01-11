977419ec82360ea1e0a8ce0631144c982e4abbe9
Yunusov321 <diloromsaidova93@gmail.com>
2025-01-11 16:22:57 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+let things = [];
+things.splice(0, 1, "non", "banan", "mashina");
+things.join('' );
+console.log(things, "n harifi bor");
+
+
+let thing = [];
+thing.splice(0, 1, "asal", "olma", "aziza", "gul")
+things.join('' );
+console.log(thing, "n harifi yok")
